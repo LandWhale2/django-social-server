cb6a6caa41c3a5bbd63943f8f3b5133c286bab23
kcuyh <khj879123@gmail.com>
2020-02-04 17:26:59 +0900

    [fix] alarm

--- story/views.py
+++ story/views.py
@@ -27,6 +27,8 @@ class StoryAlarmViewset(viewsets.ModelViewSet):
 import datetime
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from django.forms.models import model_to_dict
+import json
 
 @require_POST
 @csrf_exempt
@@ -42,7 +44,8 @@ def like(request):
         alarm_name = 'user_' + str(story_user_id)
         user_get = User.objects.get(pk= user)
         peer_nickname = user_get.nickname
-
+        
+        
         if story.likes.filter(id = user).exists():
             story.likes.remove(user)
             message = "좋아요가 취소되었습니다"
@@ -54,26 +57,20 @@ def like(request):
 
 
         
-        
         if models.StoryAlarm.objects.filter(message = story_message).exists():
-
             alarm = models.StoryAlarm.objects.get(message = story_message)
-            try:
-                #알림 모델에 해당 유저가 존재하는지 확인함
-                if alarm.nickname.filter(id = user).exists():
-                    alarm.nickname.remove(user)
-                    
-                    # 만약 모든 유저가 좋아요를 취소한다면, 에러를 발생시켜 소켓을 보내지않음
-                    if alarm.nickname.count() == 0:
-                        raise
-                    alarm.updated_ay = datetime.datetime.now()
-                    
-                else:
-                    alarm.nickname.add(user)
-                    alarm.updated_ay = datetime.datetime.now()
-
-                #소켓
-                
+            #알림 모델에 해당 유저가 존재하는지 확인함, 확인후 없으면 추가
+            if alarm.nickname.filter(id = user).exists():
+                alarm.nickname.remove(user)
+                alarm.updated_ay = datetime.datetime.now()    
+            else:
+                alarm.nickname.add(user)
+                alarm.updated_ay = datetime.datetime.now()
+            #소켓
+            if alarm.nickname.count() == 0:
+                # 만약 모든 유저가 좋아요를 취소한다면, 에러를 발생시켜 소켓을 보내지않음
+                alarm.delete()
+            else:
                 channel_layer=get_channel_layer()
                 async_to_sync(channel_layer.group_send)(
                     alarm_name,
@@ -86,10 +83,6 @@ def like(request):
                     }
                 )
                 alarm.save()
-                
-            except:
-                # 좋아요들이 취소가 되어 0이 되면 해당 모델을 삭제함
-                alarm.delete()
 
                 
         else:
